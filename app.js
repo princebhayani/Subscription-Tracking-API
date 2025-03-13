7c19fe37d2fdc56c34f110ede1e5d2ef1498c4f3
princebhayani <princebhayani192003@gmail.com>
2025-03-14 02:52:15 +0530

    create express server

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log("Start")
+import express from "express";
+
+const app = express();
+
+app.get("/",(req,res)=>{
+    res.send("Welocome to the STA");
+})
+
+app.listen(3000,()=>{
+    console.log("Server is running on http://localhost:3000");
+});
+
+
+export default app;
