e5d58545fb5901dba13ff68dabeba3044e8b2924
princebhayani <princebhayani192003@gmail.com>
2025-03-14 09:28:00 +0530

    add error handling middleware

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cookieParser from "cookie-parser";
 
 import { PORT } from "./config/env.js";
 import connectToDatabase from "./database/mongodb.js";
@@ -6,13 +7,20 @@ import connectToDatabase from "./database/mongodb.js";
 import authRouter from "./routes/auth.routes.js";
 import userRouter from "./routes/user.routes.js";
 import subscriptionRouter from "./routes/subscription.routes.js";
+import errorMiddleware from "./middlewares/error.middleware.js";
 
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(cookieParser());
+
 app.use('/api/v1/auth',authRouter);
 app.use('/api/v1/users',userRouter);
 app.use('/api/v1/subscriptions',subscriptionRouter);
+
+app.use(errorMiddleware);
 
 app.get("/",(req,res)=>{
     res.send("Welocome to the STA");
