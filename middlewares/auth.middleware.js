f03c0aea389585cfd6d880f86f13341ed5b0af3c
princebhayani <princebhayani192003@gmail.com>
2025-03-14 18:53:22 +0530

    add user authorization middleware and implement user retrieval endpoints

---
+++ middlewares/auth.middleware.js
@@ -0,0 +1,39 @@
+import jwt from "jsonwebtoken";
+import User from "../models/user.model.js";
+import { JWT_SECRET } from "../config/env.js";
+
+const authorize = async (req,res,next) => {
+    try {
+        let token;
+
+        if(req.headers.authorization && req.headers.authorization.startsWith("Bearer")){
+            token = req.headers.authorization.split(" ")[1];
+        }
+
+        if(!token){
+            return res.status(401).json({
+                success:false,
+                message:"Unauthorized access"
+            });
+        }
+        const decoded = jwt.verify(token, JWT_SECRET);
+
+        const user = await User.findById(decoded.userId);
+        if(!user){
+            return res.status(404).json({
+                success:false,
+                message:"User not found"
+            });
+        }
+        req.user = user;
+        next();
+    } catch (error) {
+        res.status(401).json({
+            success:false,
+            message:"Unauthorized access",
+            error:error.message
+        })
+    }
+};
+
+export default authorize;
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,25 +1,23 @@
 import { Router } from "express";
+import authorize from "../middlewares/auth.middleware.js";
+import { getUser, getUsers } from "../controllers/user.controller.js";
 
 const userRouter = Router();
 
-userRouter.get('/',(req,res)=>{
-    res.send({title:"GET all users"});
+userRouter.get('/', authorize, getUsers);
+
+userRouter.get('/:id', authorize, getUser);
+
+userRouter.post('/', (req, res) => {
+    res.send({ title: "CREATE new users" });
 })
 
-userRouter.get('/:id',(req,res)=>{
-    res.send({title:"GET user details"});
+userRouter.put('/:id', (req, res) => {
+    res.send({ title: "UPDATE user" });
 })
 
-userRouter.post('/',(req,res)=>{
-    res.send({title:"CREATE new users"});
-})
-
-userRouter.put('/:id',(req,res)=>{
-    res.send({title:"UPDATE user"});
-})
-
-userRouter.delete('/:id',(req,res)=>{
-    res.send({title:"DELETE user"});
+userRouter.delete('/:id', (req, res) => {
+    res.send({ title: "DELETE user" });
 })
 
 export default userRouter;
