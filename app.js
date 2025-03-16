040881831f64c0e14678f2e7cda2f946fd81d799
princebhayani <princebhayani192003@gmail.com>
2025-03-16 06:15:52 +0530

    add workflow integration for subscription reminders and configure Upstash client

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ import userRouter from "./routes/user.routes.js";
 import subscriptionRouter from "./routes/subscription.routes.js";
 import errorMiddleware from "./middlewares/error.middleware.js";
 import arcjetMiddleware from "./middlewares/arcjet.middleware.js";
+import workflowRouter from "./routes/workflow.routes.js";
 
 
 const app = express();
@@ -21,6 +22,7 @@ app.use(arcjetMiddleware);
 app.use('/api/v1/auth',authRouter);
 app.use('/api/v1/users',userRouter);
 app.use('/api/v1/subscriptions',subscriptionRouter);
+app.use('/api/v1/workflows',workflowRouter);
 
 app.use(errorMiddleware);
 
@@ -33,4 +35,4 @@ app.listen(PORT,async ()=>{
     await connectToDatabase();
 });
 
-export default app;
+export default app
